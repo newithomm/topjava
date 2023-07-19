cae97b99c2db852987070be8e7bf5009b5cafd68
new_ithomm <new_ithomm@gmx.com>
2023-07-19 22:08:15 +0300

    9_11_auth_via_user_service.patch

--- src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/ProfileRestControllerTest.java
@@ -39,7 +39,7 @@ class ProfileRestControllerTest extends AbstractControllerTest {
 
     @Test
     void update() throws Exception {
-        User updated = getUpdated();
+        UserTo updatedTo = new UserTo(null, "newName", "user@yandex.ru", "newPassword", 1500);
         perform(MockMvcRequestBuilders.put(REST_URL).contentType(MediaType.APPLICATION_JSON)
                 .with(userHttpBasic(user))
                 .content(JsonUtil.writeValue(updatedTo)))
